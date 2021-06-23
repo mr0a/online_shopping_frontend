09c36f18300f41814cdca21fb539d14d08c89403
Aravidhan <aravindhan.ct19@bitsathy.ac.in>
2021-06-23 17:12:16 +0530

    Change api address

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from 'axios'
 import store from './store'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
-// axios.defaults.baseURL = 'https://farmfrsh.herokuapp.com'
+// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://farmfrsh.herokuapp.com'
 
 createApp(App).use(store).use(router, axios).mount('#app')
